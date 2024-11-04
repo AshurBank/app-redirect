c967711545695d69107de5e5e2399430a62ea032
AshurBank <ashurbank20@gmail.com>
2024-11-04 10:04:13 +0300

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,9 @@
+<script>
+if (/android/i.test(navigator.userAgent)) {
+    window.location.href = "https://play.google.com/store/apps/details?id=com.xd.ashurbank";
+} else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    window.location.href = "https://apps.apple.com/ph/app/ashur-konnekt/id6670438193?uo=2";
+} else {
+    window.location.href = "https://ashurbank.iq/ar";
+}
+</script>
